248af3ad80af1b65fdd8d5b4abbf28decfc0454d
zzgpy1 <59162982+zzgpy1@users.noreply.github.com>
2026-05-28 18:08:52 +0800

    Update build.gradle.kts

--- app/app/build.gradle.kts
+++ app/app/build.gradle.kts
@@ -1,7 +1,7 @@
 plugins {
     id("com.android.application")
-    id("org.jetbrains.kotlin.android")
-    id("org.jetbrains.kotlin.plugin.compose")
+    kotlin("android")
+    id("org.jetbrains.compose")
 }
 
 android {
